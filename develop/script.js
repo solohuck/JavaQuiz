a0d03e211e4028509d5d99f5fe28b3077474d82c
solo <solohuckstep@gmail.com>
2022-09-22 22:02:13 -0400

    fixed question box text/title layout

---
+++ develop/script.js
@@ -0,0 +1,19 @@
+// click handler on the answer
+//data.answer 
+[
+  {
+    "question": "What is a boolean",
+    "possibleAnswers": ["A string of characters", "True or false", "A number"],
+    "correctAnswer": 1
+  },
+  {
+    "question": "What is a boolean",
+    "possibleAnswers": ["A string of characters", "True or false", "A number"],
+    "correctAnswer": 1
+  },
+  {
+    "question": "What is a boolean",
+    "possibleAnswers": ["A string of characters", "True or false", "A number"],
+    "correctAnswer": 1
+  }
+]
