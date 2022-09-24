c9760f2501459e38746c63488489022338f30bb0
solo <solohuckstep@gmail.com>
2022-09-24 19:28:27 -0400

    added footer and timer code

--- develop/script.js
+++ develop/script.js
@@ -1,19 +1,33 @@
 // click handler on the answer
 //data.answer 
-[
-  {
-    "question": "What is a boolean",
-    "possibleAnswers": ["A string of characters", "True or false", "A number"],
-    "correctAnswer": 1
-  },
-  {
-    "question": "What is a boolean",
-    "possibleAnswers": ["A string of characters", "True or false", "A number"],
-    "correctAnswer": 1
-  },
-  {
-    "question": "What is a boolean",
-    "possibleAnswers": ["A string of characters", "True or false", "A number"],
-    "correctAnswer": 1
-  }
-]
+var timerEl = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+
+
+
+
+timerCount = 60;
+var timer;
+var timerCount;
+var possibleAnswers = ["What is an example of a variable","array", "modulus", "object", "function", "string", "boolean"];
+
+function startGame() {
+  timerCount = 60;
+  startButton.disabled = true;
+  startTimer()
+}
+
+
+function startTimer() {
+  timer = setInterval(function() {
+    timerCount--;
+    timerEl.textContent = timerCount;
+    if (timerCount === 0){
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+startButton.addEventListener("click", startGame);
+init();
